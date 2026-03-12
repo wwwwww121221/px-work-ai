27ce1c4b485fbd8e9e430293f099cdddc7cdd6e0
wwwwww121221 <1499440386@qq.com>
2026-03-12 08:12:19 +0000

    PxWork 后端启动成功！

---
+++ px_work-api/src/main/java/com/pxwork/api/PxWorkApplication.java
@@ -0,0 +1,13 @@
+package com.pxwork.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PxWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PxWorkApplication.class, args);
+    }
+
+}
